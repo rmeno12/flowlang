d06776261b0e5a22038b167435a4e52f9d0868c6
Rahul Menon <menonrahul02@gmail.com>
2021-05-19 14:44:30 -0500

    tokenization

--- flow/src/main.rs
+++ flow/src/main.rs
@@ -1,17 +1,11 @@
-use std::env;
+use std::{env, fs, panic::panic_any};
 
+mod checker;
 mod token;
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    use token::Token::*;
-    let toks: Vec<token::Token> = vec![
-        StartBlock,
-        Identifier(String::from("blockname")),
-        Set,
-        Identifier(String::from("actuator")),
-        Value(50.0),
-        EndBlock,
-    ];
+    let args: Vec<String> = env::args().collect();
+    let code = fs::read_to_string(&args[1]).expect("Couldn't open file");
+    let toks = token::tokenize(code);
     println!("{:?}", toks);
 }
--- flow/src/token.rs
+++ flow/src/token.rs
@@ -3,6 +3,8 @@ pub enum Token {
     Identifier(String),
     Value(f64),
     Comparator(String),
+    Actuator,
+    Sensor,
     StartBlock,
     EndBlock,
     Set,
@@ -12,4 +14,97 @@ pub enum Token {
     Else,
     Any,
     All,
+    Tab,
+    Newline,
+    Colon,
+    ConditionStart,
+}
+
+pub fn tokenize(code: String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let words = split_code(code);
+    for word in words {
+        let to_add = match word.as_str() {
+            "sensor" => Token::Sensor,
+            "actuator" => Token::Actuator,
+            "block" => Token::StartBlock,
+            "endblock" => Token::EndBlock,
+            "set" => Token::Set,
+            "goto" => Token::Goto,
+            "wait" => Token::Wait,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "any" => Token::Any,
+            "all" => Token::All,
+            "    " => Token::Tab,
+            "\n" => Token::Newline,
+            ":" => Token::Colon,
+            "-" => Token::ConditionStart,
+            "<" | ">" | "<=" | ">=" | "=" => Token::Comparator(word),
+            _ => {
+                let chars: Vec<char> = word.chars().collect();
+                let first = chars[0];
+                if first.is_digit(10) {
+                    Token::Value(word.parse().unwrap())
+                } else {
+                    Token::Identifier(word)
+                }
+            }
+        };
+        tokens.push(to_add);
+    }
+    tokens
+}
+
+fn split_code(code: String) -> Vec<String> {
+    let mut out: Vec<String> = Vec::new();
+    let mut idx = 0;
+    let chars: Vec<char> = code.chars().collect();
+    while idx < code.chars().count() {
+        // TODO: comparators
+        match &chars[idx] {
+            '\t' | '\n' | '-' | ':' | '=' => {
+                out.push((&chars[idx]).to_string());
+                idx += 1;
+            }
+            '<' | '>' => {
+                if chars[idx + 1] == '=' {
+                    out.push((&chars[idx..idx + 2]).iter().collect());
+                    idx += 2;
+                } else {
+                    out.push((&chars[idx]).to_string());
+                    idx += 1;
+                }
+            }
+            '0'..='9' => {
+                let mut new_idx = idx;
+                while (&chars[new_idx]).is_digit(10) || chars[new_idx] == '.' {
+                    new_idx += 1;
+                }
+                out.push((&chars[idx..new_idx]).iter().collect());
+                idx = new_idx;
+            }
+            'A'..='z' => {
+                let mut new_idx = idx;
+                while (&chars[new_idx]).is_alphanumeric() || chars[new_idx] == '_' {
+                    new_idx += 1;
+                }
+                out.push((&chars[idx..new_idx]).iter().collect());
+                idx = new_idx;
+            }
+            ' ' => {
+                let nextfour: String = (&chars[idx..idx + 4]).iter().collect();
+                if nextfour.eq("    ") {
+                    out.push(String::from("    "));
+                    idx += 4;
+                } else {
+                    idx += 1;
+                }
+            }
+            _ => {
+                idx += 1;
+            }
+        }
+    }
+    out
 }
